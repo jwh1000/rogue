974eaa3dedefdb55c21da0f97c615ab448b4d1f7
jwh1000 <jwh1000@rit.edu>
2024-06-06 08:44:35 -0400

    added vi keys

--- src/EventHandlers.hpp
+++ src/EventHandlers.hpp
@@ -21,13 +21,25 @@ private:
     std::unique_ptr<Action> event_keydown(SDL_KeyboardEvent& keyEvent) {
         switch (keyEvent.keysym.sym) {
             case SDLK_UP:
+            case SDLK_k:
                 return std::make_unique<MovementAction>(0, -1);
             case SDLK_DOWN:
+            case SDLK_j:
                 return std::make_unique<MovementAction>(0, 1);
             case SDLK_LEFT:
+            case SDLK_h:
                 return std::make_unique<MovementAction>(-1, 0);
             case SDLK_RIGHT:
+            case SDLK_l:
                 return std::make_unique<MovementAction>(1, 0);
+            case SDLK_y:
+                return std::make_unique<MovementAction>(-1, -1);
+            case SDLK_u:
+                return std::make_unique<MovementAction>(1, -1);
+            case SDLK_b:
+                return std::make_unique<MovementAction>(-1, 1);
+            case SDLK_n:
+                return std::make_unique<MovementAction>(1, 1);
 
             case SDLK_ESCAPE:
                 return std::make_unique<EscapeAction>();
